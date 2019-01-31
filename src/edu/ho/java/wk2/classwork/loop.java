99a1ed3bf6da208e174f0a8ebddc75a37ea5d909
aidanho1188 <saidanho@yccc.edu>
2019-01-30 19:41:08 -0500

    1/30/2019

---
+++ src/edu/ho/java/wk2/classwork/loop.java
@@ -0,0 +1,27 @@
+package edu.ho.java.wk2.classwork;
+
+import java.util.Scanner;
+
+public class loop {
+
+	public static void main(String[] args) {
+		double balance;
+		int response;
+		int year = 1;
+		final double INT_RATE = 0.03;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter initial bank balance");
+		balance = keyboard.nextDouble();
+		keyboard.nextLine();
+		do {
+			balance = balance + balance * INT_RATE;
+			System.out.println("After year " + year + " at " + INT_RATE + " interest rate balance is $" + balance);
+			year = year + 1;
+			System.out.println("\n Do you want to see the balance " + "at the end of another year?");
+			System.out.println("Enter 1 for yes");
+			System.out.println(" or enter another number for no >>");
+			response = keyboard.nextInt();
+		} while (response == 1);
+	}
+}
